fad141d35386c290c86643490265b2dde00b9014
Rutvij Bhatt <bhatt.rut@gmail.com>
2016-09-27 22:25:42 -0400

    basic project.

--- tetris.cpp
+++ tetris.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "hello world\n";
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Red);
+
+    while (window.isOpen()) {
+	sf::Event event;
+	while (window.pollEvent(event)) {
+	    if (event.type == sf::Event::Closed) {
+		window.close();
+	    }
+	}
+	window.clear();
+	window.draw(shape); // repeatedly draw "shape"
+	window.display();
+    }
     return 0;
 }
