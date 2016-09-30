baa96d7ada7178980069f062fc8d196f92416788
Rutvij Bhatt <bhatt.rut@gmail.com>
2016-09-29 21:35:14 -0400

    able to cycle through all shapes in different colors.

--- tetris.cpp
+++ tetris.cpp
@@ -2,7 +2,35 @@
 
 #include <vector>
 #include <iostream>
+#include <array>
+#include <cassert>
 
+template<typename T>
+struct Coordinates {
+    T x;
+    T y;
+    Coordinates(const T x, const T y) : x(x), y(y) {}
+};
+
+using Offset = Coordinates<int>;
+
+const int k_NumTetrisShapes = 7;
+const int k_NumBlocksPerShape = 4;
+
+using Offsets = std::array<Offset, k_NumBlocksPerShape>;
+using TetrisShapeDef = std::array<Offsets, k_NumTetrisShapes>;
+
+const TetrisShapeDef k_Offsets = {
+{
+    { {Offset(0,0), Offset(1,0), Offset(2,0), Offset(3,0)} }, // I
+    { {Offset(0,0), Offset(1,0), Offset(2,0), Offset(1,1)} }, // T
+    { {Offset(0,0), Offset(1,0), Offset(1,1), Offset(0,1)} }, // square
+    { {Offset(0,0), Offset(0,1), Offset(0,2), Offset(1,2)} }, // L
+    { {Offset(0,0), Offset(0,1), Offset(0,2), Offset(-1,2)} }, // backwards L
+    { {Offset(0,0), Offset(0,1), Offset(-1,1), Offset(-1,2)} }, // S
+    { {Offset(0,0), Offset(0,1), Offset(1,1), Offset(1,2)} } // Z
+}
+};
 
 void move_sprites(float x_offset, std::vector<sf::Sprite *> &sprites) {
     for (auto * sprite : sprites) {
@@ -10,65 +38,137 @@ void move_sprites(float x_offset, std::vector<sf::Sprite *> &sprites) {
     }
 }
 
+std::vector<sf::Sprite> tetris_blocks(sf::Texture &texture) {
+    std::vector<sf::Sprite> sprites;
+    sprites.reserve(7);
+    // there are 7 sprites in total that we care about for each color.
+    
+    sf::Sprite purple;
+    purple.setTexture(texture);
+    purple.setTextureRect(sf::IntRect(47, 39, 50, 50));
+    sprites.push_back(purple);
+
+    sf::Sprite green;
+    green.setTexture(texture);
+    green.setTextureRect(sf::IntRect(79, 157, 50, 50));
+    sprites.push_back(green);
+
+    sf::Sprite yellow;
+    yellow.setTexture(texture);
+    yellow.setTextureRect(sf::IntRect(266, 42, 50, 50));
+    sprites.push_back(yellow);
+
+
+    sf::Sprite red;
+    red.setTexture(texture);
+    red.setTextureRect(sf::IntRect(263, 206, 50, 50));
+    sprites.push_back(red);
+
+    sf::Sprite cyan;
+    cyan.setTexture(texture);
+    cyan.setTextureRect(sf::IntRect(428, 114, 50, 50));
+    sprites.push_back(cyan);
+
+    sf::Sprite orange;
+    orange.setTexture(texture);
+    orange.setTextureRect(sf::IntRect(539, 107, 50, 50));
+    sprites.push_back(orange);
+
+    sf::Sprite magenta;
+    magenta.setTexture(texture);
+    magenta.setTextureRect(sf::IntRect(570, 216, 50, 50));
+    sprites.push_back(magenta);
+
+    return sprites;
+}
+
+enum class Color {
+    e_Purple,
+    e_Green,
+    e_Yellow,
+    e_Red,
+    e_Cyan,
+    e_Orange,
+    e_Magenta
+};
+
+class TetrisShape : public sf::Drawable {
+    std::vector<sf::Sprite> m_sprites;
+  public:
+    TetrisShape(
+	    const sf::Sprite &base_sprite,
+	    const std::array<Offset, 4> & offset_array,
+	    const Coordinates<int> &initial_coords
+	    );
+
+    virtual void draw(
+	    sf::RenderTarget &target, sf::RenderStates states) const;
+};
+
+TetrisShape::TetrisShape(
+	const sf::Sprite & base_sprite,
+	const std::array<Offset, 4> &offset_array,
+	const Coordinates<int> &initial_coords
+	) {
+    for (auto & offset : offset_array) {
+	sf::Sprite sprite(base_sprite);
+	sprite.setPosition(
+		initial_coords.x + (offset.x * 50),
+		initial_coords.y + (offset.y *50));
+	m_sprites.push_back(sprite);
+    }
+}
+
+void TetrisShape::draw(
+	sf::RenderTarget &target, sf::RenderStates states) const {
+    for (auto & sprite : m_sprites) {
+	target.draw(sprite, states);
+    }
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    sf::RenderWindow window(sf::VideoMode(400, 400), "RuTetris");
+    window.setFramerateLimit(60);
 
     sf::Texture texture;
-    if (!texture.loadFromFile("./assets/blocks.png")) {
+    if (!texture.loadFromFile("./assets/tiles.png")) {
 	throw std::runtime_error("Failed to find blocks texture file.");
     }
 
-    int height = 28, width = 28, skip = 2, start = 1;
+    auto sprites = tetris_blocks(texture);
+    assert(sprites.size() == k_Offsets.size());
 
-    std::vector<sf::Sprite> sprites;
-    for (int i = 0; i < 5; ++i) {
-	int h_offset = start + i * (width + skip);
-	sf::Sprite sprite;
-	sprite.setTexture(texture);
-	sprite.setTextureRect(sf::IntRect(h_offset, 1, width, height));
-	sprites.push_back(sprite);
+    unsigned shape_index = 0;
+
+    std::vector<TetrisShape> tetris_shapes;
+    for (unsigned i = 0; i < sprites.size(); ++i) {
+	tetris_shapes.emplace_back(
+		sprites[i], k_Offsets[i], Coordinates<int>{100,100});
     }
 
-    // make a shape - start simple, square(2x2).
-    sf::Sprite purple = sprites[0];
-    sf::Sprite cp1 = purple;
-    sf::Sprite cp2 = purple;
-    sf::Sprite cp3 = purple;
-    sf::Sprite cp4 = purple;
-    
-    cp1.setPosition(10, 10);
-    cp2.setPosition(10+width, 10);
-    cp3.setPosition(10, 10+height);
-    cp4.setPosition(10+width, 10+height);
-    std::vector<sf::Sprite *> rect{&cp1, &cp2, &cp3, &cp4};
-
+    TetrisShape * current = &tetris_shapes[shape_index++];
     while (window.isOpen()) {
 	sf::Event event;
 	while (window.pollEvent(event)) {
 	    if (event.type == sf::Event::Closed) {
 		window.close();
 	    }
-	    float x_offset = 0.f;
+	    bool change_shape = false;
 	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		x_offset = -1.f;
-	    }
-	    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		x_offset = 1.f;
+		change_shape = true;
 	    }
 
-	    // move all the sprites corresponding to the box either left or right.
-	    move_sprites(x_offset, rect);
+	    if (change_shape) {
+		std::cout << "changing shape to ";
+		if (shape_index >= k_Offsets.size()) { shape_index = 0; }
+		std::cout << shape_index << "\n";
+		current = &tetris_shapes[shape_index++];
+	    }
 	}
 
 	window.clear();
-	window.draw(cp1);
-	window.draw(cp2);
-	window.draw(cp3);
-	window.draw(cp4);
+	if (current) { window.draw(*current); }
 	window.display();
     }
     return 0;
